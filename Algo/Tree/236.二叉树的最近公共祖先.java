72b1ae68b2114d03d9c54a9fd67fb7ddc248d670
cfanr <navyifanr@gmail.com>
2022-09-11 16:22:11 +0800

    Tree

---
+++ Algo/Tree/236.二叉树的最近公共祖先.java
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=java
+ *
+ * [236] 二叉树的最近公共祖先
+ * 
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+示例 1：
+输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+输出：3
+解释：节点 5 和节点 1 的最近公共祖先是节点 3 。
+
+示例 2：
+输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+输出：5
+解释：节点 5 和节点 4 的最近公共祖先是节点 5 。因为根据定义最近公共祖先节点可以为节点本身。
+示例 3：
+
+输入：root = [1,2], p = 1, q = 2
+输出：1
+ 
+提示：
+
+树中节点数目在范围 [2, 105] 内。
+-109 <= Node.val <= 109
+所有 Node.val 互不相同 。
+p != q
+p 和 q 均存在于给定的二叉树中。
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return find(root, p.val, q.val);
+    }
+
+    TreeNode find(TreeNode root, int val1, int val2) {
+        if (root == null) {
+            return null;
+        }
+        // 要找的节点，也可能就是跟节点
+        if (root.val == val1 || root.val == val2) {
+            return root;
+        }
+        TreeNode left = find(root.left, val1, val2);
+        TreeNode right = find(root.right, val1, val2);
+        // 在左右子树中找到，则返回该跟节点
+        if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
+    }
+}
+// @lc code=end
+
